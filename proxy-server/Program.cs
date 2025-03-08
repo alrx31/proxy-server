cb070e8140f576766bfc8c837aa11809cae14085
alrx31 <usov.lesha2017@gmail.com>
2025-03-08 20:05:43 +0300

    initial commit

---
+++ proxy-server/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proxy
+{
+    class Program
+    {
+        public static string blackListFileName = "./../../../black_list.txt";
+        public static string[] blackList;
+
+        static void Main(string[] args)
+        {
+            string buf = BlackList.GetBlackList(blackListFileName);
+            blackList = buf.Trim().Split('\r', '\n');
+            Proxy.Start();
+        }
+    }
+}
